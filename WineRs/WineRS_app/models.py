bf6e2cc1ef7b06a460ed2c19d23cc0198b697245
thepema <pema7284@gmail.com>
2018-01-18 13:13:05 +0100

    Creación del modelo y de la base de datos #1

--- WineRs/WineRS_app/models.py
+++ WineRs/WineRS_app/models.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Wine(models.Model):
+	
+	country = models.CharField(max_length=50)
+	description = models.CharField(max_length=500)
+	designation = models.CharField(max_length=50)
+	points = models.CharField(max_length=10)
+	price = models.IntegerField(max_length=10)
+	province = models.CharField(max_length=50)
+	region_1 = models.CharField(max_length=50)
+	region_2 = models.CharField(max_length=50)
+	variety = models.CharField(max_length=50)
+	winery = models.CharField(max_length=50)
+	def __init__(self):
+		return self.designation
